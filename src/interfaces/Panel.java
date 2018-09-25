110855bbc1697018f683483cc79b6bc3fea8d52a
Diego Velez <36264328+Diefex@users.noreply.github.com>
2018-09-25 09:55:14 -0500

    optimizacion de paneles

--- src/interfaces/Panel.java
+++ src/interfaces/Panel.java
@@ -5,11 +5,43 @@ import javax.swing.JPanel;
 
 public class Panel extends JPanel {
     
+    private JPanel PSizq;
+    private JPanel PScen;
+    private JPanel PSder;
+    private JPanel PIizq;
+    private JPanel PIder;
+    
+    Color Green180 = new Color(134,180,4);
+    Color Orange137 = new Color(255,137,0);
+    
     public Panel () {
         
         setBounds(100,100,950,600);
-        setBackground(Color.red);
+        setBackground(new Color(230,230,230));
+        setLayout(null);
         
+        PSizq = new JPanel();
+        setPanel(PSizq, 50,50,270,200, Green180);
+        
+        PScen = new JPanel();
+        setPanel(PScen, 340,50,270,200, Orange137);
+        
+        PSder = new JPanel();
+        setPanel(PSder, 630,50,270,200, Green180);
+        
+        PIizq = new JPanel();
+        setPanel(PIizq, 50,275,460,250, Color.WHITE);
+        
+        PIder = new JPanel();
+        setPanel(PIder, 530,275,370,250, Color.WHITE);
+        
+    }
+    
+    public void setPanel (JPanel panel, int x, int y, int width, int height, Color color) {
+        panel.setBounds(x,y,width,height);
+        panel.setBackground(color);
+        panel.setLayout(null);
+        add(panel);
     }
     
 }
--- src/interfaces/Paneles.java
+++ src/interfaces/Paneles.java
@@ -15,20 +15,16 @@ public class Paneles extends JFrame {
     
     private JPanel Pizq;
     private JPanel Psup;
-    private JPanel Ppri;
-    private JPanel PSizq;
-    private JPanel PScen;
-    private JPanel PSder;
-    private JPanel PIizq;
-    private JPanel PIder;
+    
     private JPanel fondo;
     
-    private Panel p1;
+    private Panel Ppri;
     
     public Paneles () {
         super("Paneles");
         
         this.setBounds(400,250,1050,700);
+        this.setLayout(null);
         
         Color White230 = new Color(230,230,230);
         Color White220 = new Color(220,220,220);
@@ -36,60 +32,25 @@ public class Paneles extends JFrame {
         Color Green180 = new Color(134,180,4);
         Color Orange137 = new Color(255,137,0);
         
-        p1 = new Panel();
-        
-        
-        fondo = new JPanel();
-        fondo.setBounds(0,0,1050,700);
-        fondo.setBackground(Color.WHITE);
-             
-        Ppri = new JPanel();
-        Ppri.setBounds(100,100,950,600);
-        Ppri.setBackground(White230);
+        Ppri = new Panel();
         
         Pizq = new JPanel();
-        Pizq.setBounds(0,0,100,700);
-        Pizq.setBackground(Black21);
-        Pizq.setLayout(null);
+        setPanel (Pizq, 0,0,100,700, Black21);
         
         Psup = new JPanel();
-        Psup.setBounds(100,0,950,100);
-        Psup.setBackground(White220);
+        setPanel(Psup, 100,0,950,100, White220);
         
-        PSizq = new JPanel();
-        PSizq.setBounds(150,150,270,200);
-        PSizq.setBackground(Green180);
-        
-        PScen = new JPanel();
-        PScen.setBounds(440,150,270,200);
-        PScen.setBackground(Orange137);
-        
-        PSder = new JPanel();
-        PSder.setBounds(730,150,270,200);
-        PSder.setBackground(Green180);
-        
-        PIizq = new JPanel();
-        PIizq.setBounds(150,375,460,250);
-        PIizq.setBackground(Color.WHITE);
-        
-        PIder = new JPanel();
-        PIder.setBounds(630,375,370,250);
-        PIder.setBackground(Color.WHITE);
-        
-        add(PIder);
-        add(PIizq);
-        add(PSder);
-        add(PScen);
-        add(PSizq);
         add(Ppri);
-        add(Pizq);
-        add(Psup);
-        add(fondo);
-        
-        fondo.add(p1);
-        
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+    
+    public void setPanel (JPanel panel, int x, int y, int width, int height, Color color) {
+        panel.setBounds(x,y,width,height);
+        panel.setBackground(color);
+        panel.setLayout(null);
+        add(panel);
     }
     
 }
